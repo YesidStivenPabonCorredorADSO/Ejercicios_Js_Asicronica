62b05857f698fd8cb6e0f43450a3511133d64e5a
Yesid <yesidpabon145@gmail.com>
2024-05-16 00:01:40 -0500

    ,,,,

--- Ejercicios_js/Ejercicio3.js
+++ Ejercicios_js/Ejercicio3.js
@@ -1,1 +1,17 @@
-wefwef;
+// Crear una función map que acepte un array y un callback y que:
+// • por cada elemento del array ejecute el callback pasándole dicho elemento como
+// argumento
+// • obtenga el resultado
+// • lo pushee a un nuevo array
+// • devuelva el array final con el resultado de cada una de las llamadas al callback.
+
+// const prueba=[1,2,4,5]
+// const hola=prueba.map((numero)=>numero*2);
+// console.table(hpta)
+
+// const array=[1,2,4,5];
+// let w= array.map(function(callback){
+//     return callback*2
+// })
+// console.log(array)
+// console.table(w)
---
+++ Ejercicios_js/Ejericio4.js
@@ -0,0 +1,9 @@
+// Crear una función filter que acepte un array y un callback y que:
+// • por cada elemento del array ejecute el callback pasándole dicho elemento como
+// argumento
+// • sí dicho callback devuelve true, pushea el elemento a un nuevo array
+// • devuelva el array final con los elementos que pasaron el "filtro".
+const prueba=[1,2,4,6]
+let prueba_1=(prueba_1)=>prueba_1 *4
+let numero=prueba.filter(prueba_1)
+console.log(numero)
